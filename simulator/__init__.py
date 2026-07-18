149ebf7d5ffa2a3e47d1a723111417739cacdad8
ViniciusCovas <viniciuscovas@gmail.com>
2026-07-18 15:24:00 -0600

    feat: initialize exploratory match simulator package

---
+++ simulator/__init__.py
@@ -0,0 +1,5 @@
+"""Exploratory football match simulator for Synthetic XI 2026."""
+
+from .engine import MatchSimulator, SimulationConfig, simulate_many
+
+__all__ = ["MatchSimulator", "SimulationConfig", "simulate_many"]
